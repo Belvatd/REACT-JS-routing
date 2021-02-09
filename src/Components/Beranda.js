2b0ffb040c9316752703601876db0ab2001ed2ff
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2021-02-09 14:21:57 +0700

    updated-design2

--- src/Components/Beranda.js
+++ src/Components/Beranda.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class Beranda extends Component {
     render() {
         return (
             <div>
-                <h2>Ini Beranda</h2>
+                <div>
+                    <div className="home">
+                        <div className="title">
+                            <h1>Rock The Box</h1>
+                            <p className="title-sub">Rock Your Creativity Box!</p>
+                            <p className="title-p">When creativity and skills blended <br /> to the point of rocking the galaxy</p>
+                            <button>
+                                <Link to="/karya" className="text-button">GET STARTED</Link>
+                            </button>
+                        </div>
+                        <div>
+                            <p>
+                                <img className="beranda-img" src={process.env.PUBLIC_URL + "/image/" + "beranda.png"} alt="" />
+                            </p>
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>
             </div>
         )
     }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,34 +3,31 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="nav">
+                <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#"><Link className="logo" to={this.props.link1}>
                             <img src={process.env.PUBLIC_URL + "/image/" + this.props.image}
-                                className="logo" alt="logo" width="80" /></Link></a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
+                                className="logo" alt="logo" width="80" /></Link>
+                        </a>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <div className="nav">
-                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                    <p></p>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link1}>{this.props.page1}</Link></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link2}>{this.props.page2}</Link></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link3}>{this.props.page3}</Link></a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link4}>{this.props.page4}</Link></a>
-                                    </li>
-                                </ul>
-                            </div>
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <p></p>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link1}>{this.props.page1}</Link></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link2}>{this.props.page2}</Link></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link3}>{this.props.page3}</Link></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#"><Link className="nav-button" to={this.props.link4}>{this.props.page4}</Link></a>
+                                </li>
+                            </ul>
                         </div>
+
                     </div>
 
                 </nav>
